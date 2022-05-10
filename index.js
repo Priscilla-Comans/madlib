d2b53d0f9ed1d40120252d283a301da5f868902f
Priscilla-Comans <64997063+Priscilla-Comans@users.noreply.github.com>
2022-05-10 16:12:05 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var questions = 3;
+var questionsLeft = ' [' + questions + ' questions left]';
+var adjective = prompt('Please type an adjective' + questionsLeft);
+questions -= 1;
+questionsLeft = ' [' + questions + ' questions left]';
+var verb = prompt('Please type a verb that does not end in -ing ' + questionsLeft);
+questions -= 1;
+questionsLeft = ' [' + questions + ' questions left]';
+var noun = prompt('Please type a noun' + questionsLeft);
+alert('All done. Ready for the message?');
+var sentence = "<h2>There once was a  " + adjective;
+sentence += ' person who wanted to  ' + verb;
+sentence += ' the ' + noun + '.</h2>';
+document.write(sentence);
